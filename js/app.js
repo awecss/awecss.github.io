740635db5e49baa94da0c91f7fdf2074aa06136f
Naol-bm <profesornaoltena@gmail.com>
2022-09-22 16:04:32 +0300

    hammenu added and some changes

---
+++ js/app.js
@@ -0,0 +1,8 @@
+const sidebar = document.querySelector('.side-nav');
+
+function show() {
+	sidebar.style.display == 'block' ? sidebar.style.display = 'none' : sidebar.style.display = 'block'; 
+
+}
+
+
